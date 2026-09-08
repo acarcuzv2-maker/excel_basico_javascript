51b9e9da1bb241db55f72c5e9794dbaebb503811
Anthony Carcuz <acarcuzv2@miumg.edu.gt>
2026-09-08 12:46:53 -0600

    Agregar seleccion edicion y almacenamiento de celdas

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let hoja =document.getElementById("hoja");
 
 let cantidadFilas = 15
 let cantidadColumnas = 10
+let datosCeldas ={};
 
 let letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
@@ -39,12 +40,80 @@ hoja.appendChild(encabezadoFila);
 
         celda.className= "celda";
 
-hoja.appendChild(celda);
+        celda.contentEditable = true;
+
+        let nombreCelda = letras [columna -1] + fila;
+
+        celda.dataset.nombre = nombreCelda;
+
+        celda.addEventListener("click", function() {
+
+            let anterior = document.querySelector(" .celda-seleccionada");
+            
+            if (anterior) {
+            
+                anterior.classList.remove("celda-seleccionada");
+
+            }
+
+            celda.classList.add("celda-seleccionada");
+            
+            let nombre= celda.dataset.nombre;
+
+            console.log("Seleccionaste " + nombre);
+
+            if (datosCeldas [nombre]) {
+                console.log("Contenido: " + datosCeldas[nombre].contenido);
+
+            } else { 
+                console.log("La celda está vacía");
+
+            };
+
+            celda.addEventListener("input", function(){
+
+                let contenido = celda.textContent
+
+                if (contenido.trim() === ""){
+                    delete datosCeldas[celda.dataset.nombre];
+
+                    console.log(datosCeldas);
+                    return;
+                    
+                }
+
+                datosCeldas[celda.dataset.nombre] = {
+
+                        contenido: contenido
+                    };
+
+                if (contenido.startsWith("=")) { //aquí diferenciamos entre formula del demas contenido 
+                    
+                    console.log("Es una fórmula");
+                    
+                } else if (!isNaN(contenido) && contenido.trim() !== "") {
+                       
+                    console.log("Es un número");
+
+                    } else {
+                        console.log("Es un texto");
+        
+                }
+
+            }); 
+            
+            
+
+            celda.classList.add("celda-seleccionada");
+
+            console.log("Seleccionaste " + celda.dataset.nombre);
+
+            console.log(datosCeldas[celda.dataset.nombre]);
+
+        });
+
+        hoja.appendChild(celda);
 
 }
 
 }
-
-
-
-
