658094b28f0b2684bcbbd981a57e7deca7087ea4
Anthony Carcuz <acarcuzv2@miumg.edu.gt>
2026-09-09 22:03:31 -0600

    Agregar procesamiento de formulas al finalizar edición

--- script.js
+++ script.js
@@ -4,11 +4,158 @@ let cantidadFilas = 15
 let cantidadColumnas = 10
 let datosCeldas ={};
 
+function procesarFormula(contenido, celda) {
+
+                if (contenido.startsWith("=")) { //aquí diferenciamos entre formula del demas contenido 
+                    
+                    console.log("Es una fórmula");
+
+                    let formula = contenido.substring (1);
+
+                    let tokens =[];
+
+                    let tokenActual ="";
+
+                    for (let i = 0; i < formula.length; i++) {
+                        
+                        let caracter = formula[i];
+
+                        if (
+                            (caracter >= "0" && caracter <= "9") ||
+                            (caracter >= "A" && caracter <= "Z")
+
+                        ) {
+                            tokenActual = tokenActual + caracter;
+
+                            } else {
+
+                                if (tokenActual !== "") {
+
+                                    tokens.push (tokenActual);
+                                   
+                                    tokenActual= "";
+                 
+                                }
+
+                                tokens.push(caracter);
+
+                            }
+                    }
+
+                    if (tokenActual !== ""){
+
+                        tokens.push(tokenActual);
+
+                    }
+
+                    console.log(tokens);
+
+                    let tokensResueltos= [];
+
+                    for (let i= 0; i < tokens.length; i++){
+                        let token = tokens [i];
+
+                        console.log(token);
+
+                        if (!isNaN(token)){
+
+                            console.log(token + " es un número ");
+
+                            tokensResueltos.push(token);
+                            
+                        }
+
+                        else if (
+                            token === "+" ||
+                            token === "-" ||
+                            token === "*" ||
+                            token === "/" 
+                        ) {
+                            console.log (token + " es un operador");
+
+                            tokensResueltos.push(token);
+
+                        }
+                        else {
+                            console.log(token + " es una referencia");
+
+                            if (datosCeldas[token])  {
+
+                                let valorReferencia = datosCeldas[token].contenido;
+
+                                console.log(
+
+                                    "El valor de " + token + " es " + valorReferencia
+                                    
+                                );
+
+                                tokensResueltos.push(valorReferencia);
+
+                            } else{
+
+                                console.log(token + " está vacía");
+
+                                tokensResueltos.push("0");
+
+                            }
+
+                        }
+
+                        }
+
+                        console.log("Tokens resueltos:", tokensResueltos);
+
+                        let numero1 = Number(tokensResueltos[0]);
+
+                        let operador = tokensResueltos[1];
+
+                        let numero2 = Number(tokensResueltos[2]);
+
+                        let resultado;
+
+                        if (operador === "+") {
+
+                        resultado = numero1 + numero2;
+
+                        } else if (operador === "-") {
+
+                            resultado = numero1 - numero2;
+
+                        } else if (operador === "*") {
+
+                            resultado = numero1 * numero2;
+
+                        } else if (operador === "/") {
+
+                            resultado = numero1 / numero2;
+
+                        }
+
+                        console.log("Resultado final: " + resultado);
+
+                        datosCeldas[celda.dataset.nombre].valor = resultado;
+
+                        celda.textContent = resultado;
+
+
+                        
+
+                } else if (!isNaN(contenido) && contenido.trim() !== "") {
+                       
+                    console.log("Es un número");
+
+                    } else {
+                        console.log("Es un texto");
+        
+                }
+
+}
+
 let letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
 let esquina = document.createElement("div");
 
-esquina.classname = "encabezado";
+esquina.className = "encabezado";
 
 hoja.appendChild(esquina);
 
@@ -28,7 +175,7 @@ for (let fila= 1; fila<= cantidadFilas; fila++){
 
 let encabezadoFila =document.createElement("div");
 
-encabezadoFila.classname="encabezado";
+encabezadoFila.className="encabezado";
 
 encabezadoFila.textContent=fila;
 
@@ -67,53 +214,44 @@ hoja.appendChild(encabezadoFila);
 
             } else { 
                 console.log("La celda está vacía");
+            }
 
-            };
+            });
 
             celda.addEventListener("input", function(){
 
                 let contenido = celda.textContent
 
                 if (contenido.trim() === ""){
+
                     delete datosCeldas[celda.dataset.nombre];
 
                     console.log(datosCeldas);
+
                     return;
-                    
+
                 }
 
                 datosCeldas[celda.dataset.nombre] = {
 
-                        contenido: contenido
+                        contenido: contenido,
+
+                        valor: contenido
                     };
+                
+            });        
 
-                if (contenido.startsWith("=")) { //aquí diferenciamos entre formula del demas contenido 
-                    
-                    console.log("Es una fórmula");
-                    
-                } else if (!isNaN(contenido) && contenido.trim() !== "") {
-                       
-                    console.log("Es un número");
+            celda.addEventListener("blur", function() {
 
-                    } else {
-                        console.log("Es un texto");
-        
+                let contenido = celda.textContent;
+
+                if (contenido.startsWith("=")) {
+                    procesarFormula(contenido, celda);
                 }
 
-            }); 
-            
-            
-
-            celda.classList.add("celda-seleccionada");
-
-            console.log("Seleccionaste " + celda.dataset.nombre);
-
-            console.log(datosCeldas[celda.dataset.nombre]);
-
-        });
+            });
 
         hoja.appendChild(celda);
-
-}
-
-}
+        }
+        }
+        
